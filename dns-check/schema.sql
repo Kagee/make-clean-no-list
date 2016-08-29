5851510237146433928fdaf825a12fc331838387
Anders Einar Hilden <hildenae@gmail.com>
2016-08-29 17:41:44 +0200

    rename in schema

--- dns-check/schema.sql
+++ dns-check/schema.sql
@@ -1,12 +1,10 @@
 create table potential_domains (
     id             serial      primary key,
     first_inserted timestamp   not null default now(),
-    last_checked   timestamp   default null,
+    last_found     timestamp   default null,
     name           text        not null unique, -- sol.no, www.xn--troms-zua.no
     source         text        not null
 );
-
-CREATE INDEX potential_domains_last_checked ON potential_domains (last_checked);
 
 create table found (
     id          serial      primary key,
@@ -17,13 +15,15 @@ create table found (
 );
 
 create table servfails (
-    id          serial      primary key,
-    domain_id   int         not null references domains(id),
-    timestamp   timestamp   not null default now()
+    id            serial      primary key,
+    domain_id     int         not null references potential_domains(id),
+    last_checked  timestamp   not null default now()
 );
 
 create table dns_timeouts (
-    id          serial      primary key,
-    domain_id   int         not null references domains(id),
-    timestamp   timestamp   not null default now()
+    id            serial      primary key,
+    domain_id     int         not null references potential_domains(id),
+    last_checked  timestamp   timestamp   not null default now()
 );
+
+CREATE INDEX potential_domains_last_checked ON potential_domains (last_checked);
